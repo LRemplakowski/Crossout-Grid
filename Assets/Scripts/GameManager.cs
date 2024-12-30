262545bfac1e9bcbc1351d02855d584b5afd6747
Switchu <remplak@gmail.com>
2024-12-30 12:59:11 +0100

    game manager wip

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Crossout.Core
+{
+    public class GameManager : MonoBehaviour
+    {
+        public void BeginGame()
+        {
+
+        }
+    }
+}
